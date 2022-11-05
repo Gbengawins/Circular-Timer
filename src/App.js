30affee86b4fecb62f6faa48314fc57cb2dcbdc6
Victor Ajagunna <victor-ajagunna@hotmail.com>
2022-11-05 20:27:34 +0100

    project setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import "./index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App () {
+  //Start time
+  const startTime = 10000;
+
+  //Remaining time value
+  const [ timeLeft, setTimeLeft ] = useState(startTime);
+  
+  //Timer State - started or paused
+  const [ isPause, setIsPaused ] = useState(false);
+  
+  //Progressive State - started or not
+  const [ isStarted, setStarted ] = useState(false);
+  
+  useEffect(() => { 
+
+    const countdown = (() => {
+      if (isStarted && !isPaused) {
+      set
+    }
+  }
+      
+  });
+  return <div className="App"></div>;
 }
 
 export default App;
