eb046b22a5492f46f53f06b21e830a4d7a545afb
Victor Ajagunna <victor-ajagunna@hotmail.com>
2022-11-05 22:05:37 +0100

    Styling commit

--- src/App.js
+++ src/App.js
@@ -12,21 +12,83 @@ function App () {
   const [ timeLeft, setTimeLeft ] = useState(startTime);
   
   //Timer State - started or paused
-  const [ isPause, setIsPaused ] = useState(false);
+  const [ isPaused, setIsPaused ] = useState(false);
   
   //Progressive State - started or not
-  const [ isStarted, setStarted ] = useState(false);
+  const [ isStarted, setIsStarted ] = useState(false);
   
   useEffect(() => { 
 
-    const countdown = (() => {
+    const countdown = () => {
       if (isStarted && !isPaused) {
-      set
-    }
-  }
+        setTimeLeft(timeLeft - 1000);
+      }
+    };
+
+    const timer = setTimeout(countdown, 1000);
+
+    if (isStarted === false) { 
+      setTimeLeft(startTime);
+      return clearTimeout(timer);
+    };
       
-  });
-  return <div className="App"></div>;
+      if (timeLeft === 0) {
+        return clearTimeout(timer);
+      }
+    }, [ isStarted, timeLeft, isPaused ]);
+
+    const getMinutes = () => {
+      let minutes = Math.floor((timeLeft / 1000 / 60) % 60);
+      
+      if (minutes < 10) {
+        minutes = "0" + minutes;
+      }
+      return minutes;
+    };
+
+    //Convert time left to seconds
+    const getSeconds = () => {
+      let seconds = Math.floor((timeLeft / 1000) % 60);
+
+      if (seconds < 10) {
+        seconds = "0" + seconds;
+      }
+      return seconds;
+    };
+
+    //Reset values
+    const resetTimer = () => { 
+      setIsPaused(false);
+      setIsStarted(false);
+    };
+      
+
+  return (
+    <div className="app">
+      <div className="clock-section">
+        <div className="clock-container">
+          <CircularProgressbar
+            maxValue={ startTime }
+            value={ startTime - timeLeft }
+            text={ `${getMinutes()}:${getSeconds()}` }
+          />
+        </div>
+        <div className="button-section">
+          { isStarted ? (
+            <>
+              <button onClick={ resetTimer }>Reset</button>
+              <button onClick={ () => setIsPaused(!isPaused) }>
+                {isPaused ? "Resume" : "Pause" }
+              </button>
+            </>
+          ) : (
+              <button onClick={() => setIsStarted(true) }>Start</button>
+          )}
+        </div>
+      </div>
+    </div>
+    
+  );
 }
 
 export default App;
